da6271f338dc0cacf5c1db78514a84bfcc0ad22f
fialot <fialot@atlas.cz>
2023-05-10 14:33:36 +0200

    First

---
+++ App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace NetworkManager;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace NetworkManager;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
