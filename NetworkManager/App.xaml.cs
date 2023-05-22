054eada9c4a81f45c8226a23878202d7da65e1b0
fialot <fialot@atlas.cz>
2023-05-22 08:50:26 +0200

    Add gitignore

--- NetworkManager/App.xaml.cs
+++ NetworkManager/App.xaml.cs
@@ -67,7 +67,6 @@ public partial class App : Application
             services.AddSingleton<INavigationService, NavigationService>();
 
             // Core Services
-            services.AddSingleton<ISampleDataService, SampleDataService>();
             services.AddSingleton<IFileService, FileService>();
 
             // Views and ViewModels
